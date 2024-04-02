f3f6634d7f5124018d716eff0a4b2622eab54420
ilborsch <ilborshch@illias-air.home>
2024-04-02 21:42:29 +0100

    Updated handlers

--- internal/handlers/auth/handler.go
+++ internal/handlers/auth/handler.go
@@ -19,13 +19,26 @@ type Handler struct {
 }
 
 func (h *Handler) Login(ctx context.Context, email, password string, appID int32) (string, error) {
-	// TODO: 1:52:18
+	log := h.log.With(
+		slog.String("from", logFrom+".Login"),
+		slog.String("email", email),
+	)
+	user, err := h.UserProvider.User(ctx, email)
+	if err != nil {
+		log.Error("error retrieving user from storage " + err.Error())
+		return "", status.Error(codes.Internal, "internal error")
+	}
+	if err := bcrypt.CompareHashAndPassword(user.PasswordHashed, []byte(password)); err != nil {
+		log.Error("password and hashed passwords don't match " + err.Error())
+		return "", status.Error(codes.InvalidArgument, "wrong credentials")
+	}
+	// TODO: 1:56:39
 	panic("implement me")
 }
 
 func (h *Handler) RegisterUser(ctx context.Context, email, password string) (int64, error) {
 	log := h.log.With(
-		slog.String("from", logFrom+".Login"),
+		slog.String("from", logFrom+".RegisterUser"),
 		slog.String("email", email),
 	)
 	passHashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
